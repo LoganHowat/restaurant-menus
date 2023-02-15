4e7d6163faa3aa5f358b67781290ec70c2e01d24
LoganHowat <lhowat624@hotmail.com>
2023-02-15 16:34:04 +0000

    Final commit

--- index.test.js
+++ index.test.js
@@ -18,26 +18,59 @@ describe('Restaurant and Menu Models', () => {
 
     test('can create a Restaurant', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        let restaurant = Restaurant.create({
+            name: 'McDonalds',
+            location: 'London',
+            cuisine: 'Fast-food'
+        });
+        expect((await restaurant).dataValues.name).toBe('McDonalds')
+        expect((await restaurant).dataValues.location).toBe('London')
+        expect((await restaurant).dataValues.cuisine).toBe('Fast-food')
     });
 
     test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        let menu = Menu.create({
+            title: 'MenuTitle',
+        });
+        console.log(await menu)
+        expect((await menu).dataValues.title).toBe('MenuTitle')
     });
 
     test('can find Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        let foundRes = await Restaurant.findAll({
+            where:{
+                name:"McDonalds"
+            }
+        })
+        expect(foundRes[0]['name']).toBe('McDonalds');
+        expect(foundRes[0]['location']).toBe('London');
+        expect(foundRes[0]['cuisine']).toBe('Fast-food');
     });
 
     test('can find Menus', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        let foundMenu = await Menu.findAll({
+            where:{
+                title:"MenuTitle"
+            }
+        })
+        expect(foundMenu[0]['title']).toBe('MenuTitle');
     });
 
     test('can delete Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        let restaurant2 = Restaurant.create({
+            name: 'Test',
+            location: 'Test_location',
+            cuisine: 'Test_cuisine'
+        });
+        (await restaurant2).destroy();
+        let foundTest = await Restaurant.findAll({
+            where:{
+                name: "Test"
+            }
+        })
+        expect(foundTest.length).toBe(0);
     });
 })
